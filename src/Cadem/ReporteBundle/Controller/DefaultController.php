436a90beb1af06ca589907a52edb407d40bc7bcb
jonsxaero <jonsxaero@gmail.com>
2013-03-28 11:55:24 -0300

    Se agregan filtros, ranking y su efecto

--- src/Cadem/ReporteBundle/Controller/DefaultController.php
+++ src/Cadem/ReporteBundle/Controller/DefaultController.php
@@ -12,12 +12,56 @@ class DefaultController extends Controller
 	public function indexAction()
     {
 		$defaultData = array();
-		$form = $this->createFormBuilder($defaultData)
-			->add('Filtro1', 'choice', array(
-				'choices'   => array('d1' => 'Dato1', 'd2' => 'Dato2', 'd3' => 'Dato3'),
+		$form_agencia = $this->createFormBuilder($defaultData)
+			->add('Agencia', 'choice', array(
+				'choices'   => array(
+						'1' => 'ANTOFAGASTA',
+						'2' => 'COQUIMBO',
+						'3' => 'VALPARAISO',
+						'4' => 'METROPOLITANA'
+				),
 				'required'  => true,
 				'multiple'  => true,
-				'data' => array('d1','d2','d3')				
+				'data' => array('1','2','3','4')				
+			))
+			->getForm();
+		$form_sala = $this->createFormBuilder($defaultData)
+			->add('Sala', 'choice', array(
+				'choices'   => array(
+						'1' => 'STA ISABEL PADRE LAS CASAS',
+						'2' => 'SOC.KAROMEHI LIMITAD',
+						'3' => 'COMERCIALIZADORA ANDREANI LTDA',
+						'4' => 'SUAREZ Y CIA LTDA',
+						'5' => 'PANIFICADORA LA EURO'
+				),
+				'required'  => true,
+				'multiple'  => true,
+				'data' => array('1','2','3','4','5')				
+			))
+			->getForm();
+			
+		$form_sku = $this->createFormBuilder($defaultData)
+			->add('SKU', 'choice', array(
+				'choices'   => array(
+						'1' => 'JABON',
+						'2' => 'SHAMPOO',
+						'3' => 'MANTEQUILLA'
+				),
+				'required'  => true,
+				'multiple'  => true,
+				'data' => array('1','2','3')				
+			))
+			->getForm();
+			
+		$form_categoria = $this->createFormBuilder($defaultData)
+			->add('Categoria', 'choice', array(
+				'choices'   => array(
+						'1' => 'LIMPIEZA',
+						'2' => 'ALIMENTO'
+				),
+				'required'  => true,
+				'multiple'  => true,
+				'data' => array('1','2')				
 			))
 			->getForm();
 		
@@ -37,7 +81,17 @@ class DefaultController extends Controller
 		$logos = $cliente->getLogos();
 		$variables_clientes = $cliente->getVariablesClientes();
 
-        return $this->render('CademReporteBundle:Default:index.html.twig', array('form' => $form->createView(), 'logo' => $logos[0], 'variables_clientes' => $variables_clientes ));
+        return $this->render('CademReporteBundle:Default:index.html.twig',
+		array(
+			'forms' => array(
+				'form_agencia' => $form_agencia->createView(),
+				'form_sala' => $form_sala->createView(),
+				'form_sku' => $form_sku->createView(),
+				'form_categoria' => $form_categoria->createView()
+			),
+			'logo' => $logos[0],
+			'variables_clientes' => $variables_clientes )
+		);
     }
 	
 	public function indicadoresAction(Request $request)
@@ -55,7 +109,6 @@ class DefaultController extends Controller
 		// $data = $form->getData();
 		
 		$data = $request->request->all();
-        //return $this->render('CademReporteBundle:Default:indicadores.html.twig', array('data' => $data['form']));
 		$responseA = array( 
 				'cobertura' =>	array(
 					'type' => 'pie',
@@ -72,6 +125,32 @@ class DefaultController extends Controller
 							array('name' => 'Cumple', 'y' => 35.5, 'color' => '#83A931'),
 							array('name' => 'No cumple', 'y' => 64.5, 'color' => '#EB3737')
 						)
+				),
+				'quiebre' =>	array(
+					'type' => 'pie',
+					'name' => 'Quiebre',
+					'data' => array(
+							array('name' => 'Cumple', 'y' => 55.5, 'color' => '#83A931'),
+							array('name' => 'No cumple', 'y' => 44.5, 'color' => '#EB3737')
+						)
+				),
+				'presencia' =>	array(
+					'type' => 'pie',
+					'name' => 'Presencia',
+					'data' => array(
+							array('name' => 'Cumple', 'y' => 44.5, 'color' => '#83A931'),
+							array('name' => 'No cumple', 'y' => 55.5, 'color' => '#EB3737')
+						)
+				),
+				'ranking' => array(
+					'head' => array(
+						'ID', 'SALA', 'PROM', 'PROM ANT', 'DIF'
+					),
+					'body' => array(
+						array('1','STA ISABEL', '91%', '90%', '1'),
+						array('2','SUEAREZ Y CIA', '71%', '90%', '-29'),
+						array('3','LIDER', '81%', '90%', '-9')
+					)
 				)
 		);
 		$responseB = array( 
@@ -90,9 +169,25 @@ class DefaultController extends Controller
 							array('name' => 'Cumple', 'y' => 15.5, 'color' => '#83A931'),
 							array('name' => 'No cumple', 'y' => 84.5, 'color' => '#EB3737')
 						)
+				),
+				'quiebre' =>	array(
+					'type' => 'pie',
+					'name' => 'Quiebre',
+					'data' => array(
+							array('name' => 'Cumple', 'y' => 0, 'color' => '#83A931'),
+							array('name' => 'No cumple', 'y' => 100, 'color' => '#EB3737')
+						)
+				),
+				'presencia' =>	array(
+					'type' => 'pie',
+					'name' => 'Presencia',
+					'data' => array(
+							array('name' => 'Cumple', 'y' => 44.5, 'color' => '#83A931'),
+							array('name' => 'No cumple', 'y' => 55.5, 'color' => '#EB3737')
+						)
 				)
 		);
-		if(in_array('d1', $data['form']['Filtro1'])) return new JsonResponse($responseA);
+		if(in_array('1', $data['form']['Agencia'])) return new JsonResponse($responseA);
 		else return new JsonResponse($responseB);
 		
     }
